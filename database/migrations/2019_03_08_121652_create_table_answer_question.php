45cc258a413eea992f382f3b8d4f00ecdab92480
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-03-08 15:20:38 +0300

    Database Model :smiley:

---
+++ database/migrations/2019_03_08_121652_create_table_answer_question.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableAnswerQuestion extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(
+            'answer_question',
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('question_id')->unsigned();
+                $table->integer('answer_id')->unsigned();
+                $table->foreign('answer_id')->references('id')->on('questions');
+                $table->foreign('question_id')->references('id')->on('answers');
+                $table->timestamps();
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_answer_question');
+    }
+}
