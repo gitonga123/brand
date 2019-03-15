06cac57a44b37f50c8fe20655af56404de228ecb
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-03-15 15:57:11 +0300

    Correct Answers :+1:

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Get all the roles attaching up to 3 random roles to each user
+        // Get all the Questions attaching up to 3 random answers to each user
         $answers = App\Answer::all();
 
         // Populate the pivot table
@@ -19,6 +19,18 @@ class DatabaseSeeder extends Seeder
             function ($question) use ($answers) {
                 $question->answers()->attach(
                     $answers->random(rand(1, 5))->pluck('id')->toArray()
+                );
+            }
+        );
+
+        // Get all the question attaching up to 3 random hints to each user
+        $hints = App\Hint::all();
+
+        // Populate the pivot table
+        App\Question::all()->each(
+            function ($question) use ($hints) {
+                $question->hints()->attach(
+                    $hints->random(rand(1, 5))->pluck('id')->toArray()
                 );
             }
         );
